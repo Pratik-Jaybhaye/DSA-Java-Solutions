090b096b7f3f47b75f9fc31b91bc073a4b6caa3b
pratik <pratikjaybhaye.sit.it@gmail.com>
2026-02-09 11:51:15 +0530

    updated corner cases for negative numbers

--- arrays/SecondLargestElement.java
+++ arrays/SecondLargestElement.java
@@ -10,9 +10,9 @@ logic:
 */
 public class SecondLargestElement {
     public static int secondLargest(int arr[]){
-        int n=arr.length;
-        int largest=-1;
-        int secondLargest=-1;
+       
+        int largest=Integer.MIN_VALUE;
+        int secondLargest=Integer.MIN_VALUE;
 
         for(int i=0;i<arr.length;i++){
             if(arr[i]>largest){
@@ -26,7 +26,7 @@ public class SecondLargestElement {
             return secondLargest;
     }
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,5};
+        int arr[]={0,-2,-3,-4,-5};
           
         System.out.println(SecondLargestElement.secondLargest(arr) );
     }
